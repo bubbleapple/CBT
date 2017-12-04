b0ca0eee9eba9bd15e140b0412a7cc659678f26e
Lei Ju <mrleiju@users.noreply.github.com>
2017-12-03 22:30:03 -0500

    incorporated bootstrap package

--- js/script.js
+++ js/script.js
@@ -2,13 +2,17 @@ V = new Set();
 
 paper.install(window)
 window.onload = function() {
-	var canvas = document.getElementById('inputCanvas');
+// 	var canvas = document.getElementById('inputCanvas');
 	var mstCanvas = document.getElementById('mstCanvas');
 	var sptCanvas = document.getElementById('sptCanvas');
 	var freeCanvas = document.getElementById('freeCanvas');
-	var slider = document.getElementById("myRange");
+	var sliderSPT = document.getElementById("mySPTRange");
+    var sliderBRBC = document.getElementById("myBRBCRange");
 
-	paper.setup(canvas);
+    paper.setup(mstCanvas);
+	paper.setup(sptCanvas);
+    paper.setup(freeCanvas);
+
 
 // 	paper.setup(MSTCanvas);
 	var tool = new Tool();
@@ -19,19 +23,20 @@ window.onload = function() {
 
 	tool.onMouseDown = function(event) {
 		var temp = new MyPoint([event.point.x, event.point.y]);
+        console.log(event.target);
 		console.log(temp);
-		if(V.size == 0) source = temp;
+		if(V.size == 0) 
+            source = temp;
 		V.add(temp);
 
-		plot_points_in(V, canvas);
-
+// 		plot_points_in(V, canvas);
 		plot_mst(V, source, mstCanvas);
 		plot_spt(V, source, sptCanvas);
-		plot_freetree(V, source, slider.value/100.0, freeCanvas);
+		plot_freetree(V, source, sliderSPT.value/100.0, freeCanvas);
 	}
 
-	slider.oninput = function() {
+	sliderSPT.oninput = function() {
         // console.log(slider.value);
-		plot_freetree(V, source, slider.value/100.0, freeCanvas);
+		plot_freetree(V, source, sliderSPT.value/100.0, freeCanvas);
 	}
 }
