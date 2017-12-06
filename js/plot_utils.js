e6acf83568c8502c90b1e3ff737f3b8abfb9c3b1
Lei Ju <mrleiju@users.noreply.github.com>
2017-12-05 20:21:42 -0500

    mark all the canvas's source red

--- js/plot_utils.js
+++ js/plot_utils.js
@@ -14,27 +14,27 @@ function plot_point(pnt, color = 'black') {
     myCircle.fillColor = color;
 }
 
-// FUNCTION: plot_tree(set)
-//      plot the set on the current canvas
-// PARAMETERS:
-//      set: set of points(MST_SPT.Point)
-// RETURN:
-//      void
-//
-function plot_tree(set) {
-    for(let pnt of set) {
-        // plot the point
-        plot_point(pnt);
-
-        // plot the edge
-        for(let pnt1 of pnt.neighbors) {
-            var path = new paper.Path();
-            path.strokeColor = 'black';
-            path.add(new paper.Point(pnt.vector[0],pnt.vector[1]));
-            path.add(new paper.Point(pnt1.vector[0],pnt1.vector[1]));
-        }
-    }
-}
+// // FUNCTION: plot_tree(set)
+// //      plot the set on the current canvas
+// // PARAMETERS:
+// //      set: set of points(MST_SPT.Point)
+// // RETURN:
+// //      void
+// //
+// function plot_tree(set) {
+//     for(let pnt of set) {
+//         // plot the point
+//         plot_point(pnt);
+// 
+//         // plot the edge
+//         for(let pnt1 of pnt.neighbors) {
+//             var path = new paper.Path();
+//             path.strokeColor = 'black';
+//             path.add(new paper.Point(pnt.vector[0],pnt.vector[1]));
+//             path.add(new paper.Point(pnt1.vector[0],pnt1.vector[1]));
+//         }
+//     }
+// }
 
 // FUNCTION: plot_points(set)
 //      plot all the points in the set on the current canvas.
@@ -59,28 +59,8 @@ function plot_points(set) {
 // RETURN:
 //      void
 //
-function plot_wrapper(set, canvas, func) {
-	// Create an empty project and a view for the canvas:
+function plot_wrapper(vertices, canvas, source) {
 	paper.setup(canvas);
-
-    func(set);
-	// Draw the view now:
-	paper.view.draw();
-}
-
-// FUNCTION: plot_mst(set, source, canvasId)
-//      plot the mst tree from the set, started at source on the canvas of canvasId
-// PARAMETERS:
-//      set: set of points(MST_SPT.Point)
-//      source: assigned source for the mst Tree
-//      canvas: canvas object from dom.
-// RETURN:
-//      void
-//
-function plot_mst(set, source, canvas) {
-    paper.setup(canvas);
-    let vertices = PrimsMST(set, source);
-
     for(let pnt of vertices){
         // plot the point
         if(arrayEqual(pnt.vector, source.vector))
@@ -96,9 +76,21 @@ function plot_mst(set, source, canvas) {
             path.add(new paper.Point(pnt1.vector[0],pnt1.vector[1]));
         }
     }
-
     paper.view.draw();
 }
+
+// FUNCTION: plot_mst(set, source, canvasId)
+//      plot the mst tree from the set, started at source on the canvas of canvasId
+// PARAMETERS:
+//      set: set of points(MST_SPT.Point)
+//      source: assigned source for the mst Tree
+//      canvas: canvas object from dom.
+// RETURN:
+//      void
+//
+function plot_mst(set, source, canvas) {
+    plot_wrapper(PrimsMST(set, source), canvas, source);
+ }
 
 // FUNCTION: plot_spt(set, source, canvasId)
 //      plot the spt tree from the set, started at source on the canvas of canvasId
@@ -110,27 +102,15 @@ function plot_mst(set, source, canvas) {
 //      void
 //
 function plot_spt(set, source, canvas) {
-    plot_wrapper(SPT(set, source), canvas, plot_tree);
+    plot_wrapper(SPT(set, source), canvas, source);
 }
 
 function plot_freetree(set, source, value, canvas) {
-    plot_wrapper(getTree(set, source, value), canvas, plot_tree);
-}
-
-// FUNCTION: plot_points_in(set, canvas)
-//      plot all the points from the set
-// PARAMETERS:
-//      set: set of points(MST_SPT.Point)
-//      canvas: canvas object from dom.
-// RETURN:
-//      void
-//
-function plot_points_in(set, canvas) {
-    plot_wrapper(set, canvas, plot_points);
+    plot_wrapper(getTree(set, source, value), canvas, source);
 }
 
 
 // TODO: annotation
 function plot_brbc(set, source, value, canvas) {
-    plot_wrapper(BRBCTree(set, source, value), canvas, plot_tree);
+    plot_wrapper(BRBCTree(set, source, value), canvas, source);
 }
